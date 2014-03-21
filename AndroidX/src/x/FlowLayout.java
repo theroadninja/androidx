2af9de9e263534d87af96f72ed58bc08fc3739ac
Max Tower <max@maxtower.com>
2014-03-20 17:33:23 -0700

    Added vertical support to FlowLayout FlowLayout now extends LinearLayout

--- AndroidX/src/x/FlowLayout.java
+++ AndroidX/src/x/FlowLayout.java
@@ -9,17 +9,10 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.MeasureSpec;
+import android.widget.LinearLayout;
 
-/**
- * 
- * 
- * TODO:  horizontal vs vertical
- * 
- * @author Dave
- *
- */
-public class FlowLayout extends ViewGroup {
-	
+public class FlowLayout  extends LinearLayout {
 	private static final String TAG = "FlowLayout";
 	private void log(String message){
 		Log.e(TAG, message);
@@ -39,7 +32,7 @@ public class FlowLayout extends ViewGroup {
 	private int orientation = HORIZONTAL;
 	
 	*/
-	
+	private int orientation = HORIZONTAL;
 	/** do not rely on the size of this array to indicate how many rows exist */
 	private ArrayList<Integer> measuredRowHeights = new ArrayList<Integer>();
 	/** used by onLayout() */
@@ -49,11 +42,12 @@ public class FlowLayout extends ViewGroup {
 
 	public FlowLayout(Context context) {
 		super(context);
+		orientation = getOrientation();
 	}
 	
 	public FlowLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		
+		orientation = getOrientation();
 		/*
 		 * Parsing attributes is going to be a problem
 		 * -we cant access the LinearLayout attributes
@@ -63,29 +57,42 @@ public class FlowLayout extends ViewGroup {
 		 */
 	}
 	
-	
-	@Override
-	protected boolean checkLayoutParams(ViewGroup.LayoutParams lp){
-		return lp != null && MarginGravityLayoutParams.class.isAssignableFrom(lp.getClass());
-	}
-	
-	@Override
-	protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp){
-		return new ViewGroup.LayoutParams(lp);
-	}
-	
 
-	
-	@Override
-	public MarginGravityLayoutParams generateLayoutParams(AttributeSet attrs){
-		
-		return new MarginGravityLayoutParams(getContext(), attrs);
-	}
-	
-	@Override
-	protected ViewGroup.LayoutParams generateDefaultLayoutParams(){
-		return new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-	}
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LinearLayout.LayoutParams(getContext(), attrs);
+    }
+
+    /**
+     * Returns a set of layout parameters with a width of
+     * {@link android.view.ViewGroup.LayoutParams#FILL_PARENT}
+     * and a height of {@link android.view.ViewGroup.LayoutParams#WRAP_CONTENT}
+     * when the layout's orientation is {@link #VERTICAL}. When the orientation is
+     * {@link #HORIZONTAL}, the width is set to {@link LayoutParams#WRAP_CONTENT}
+     * and the height to {@link LayoutParams#WRAP_CONTENT}.
+     */
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        if (orientation == HORIZONTAL) {
+            return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        } else if (orientation == VERTICAL) {
+            return new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        }
+        return null;
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new LayoutParams(p);
+    }
+
+
+    // Override to allow type-checking of LayoutParams.
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof LinearLayout.LayoutParams;
+    }
+    
 	
 	/**
 	 * According to http://developer.android.com/reference/android/view/ViewGroup.html#onLayout%28boolean,%20int,%20int,%20int,%20int%29
@@ -102,9 +109,106 @@ public class FlowLayout extends ViewGroup {
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
-		onMeasureHorizontal(widthMeasureSpec, heightMeasureSpec);
+		if(orientation == HORIZONTAL)
+		{
+		   onMeasureHorizontal(widthMeasureSpec, heightMeasureSpec);
+		}
+		else
+	    {
+			onMeasureVertical(widthMeasureSpec, heightMeasureSpec);
+		}
 	}
 	
+	private void onMeasureVertical(int widthMeasureSpec, int heightMeasureSpec){
+		
+		//TODO: implement padding
+		final int paddingW = getPaddingLeft() + getPaddingRight();
+		final int paddingH = getPaddingTop() + getPaddingBottom();
+		
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+		
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+			
+		int rowIndex = 0;
+		
+		int currentRowHeight = 0;
+		int maxRowHeight = 0;
+		
+		measuredRowHeights.clear();
+		
+		for(int i = 0; i < getChildCount(); ++i){
+			View child = getChildAt(i);
+			if(child.getVisibility() == View.GONE) continue;
+			
+			//MarginGravityLayoutParams childLp = MarginGravityLayoutParams.fromLayoutParams(child.getLayoutParams());
+			LinearLayout.LayoutParams childLp = (LinearLayout.LayoutParams) child.getLayoutParams();
+			
+			
+			//int childWidthSpec = getHorizontalChildWidthSpec(widthMode, widthSize, childLp);
+			int childWidthSpec = ViewGroup.getChildMeasureSpec(widthMeasureSpec, paddingW, childLp.width);
+			int childHeightSpec = ViewGroup.getChildMeasureSpec(heightMeasureSpec, paddingH, childLp.height);
+			
+			child.measure(childWidthSpec, childHeightSpec);
+			
+			
+			int w = child.getMeasuredWidth() + childLp.leftMargin + childLp.rightMargin;
+			int h = child.getMeasuredHeight() + childLp.bottomMargin + childLp.topMargin;
+			boolean newRow = (heightMode != MeasureSpec.UNSPECIFIED
+					&& currentRowHeight + h + paddingH > heightSize);
+			
+			
+			//the +1 just makes is easier when we increment row index
+			while(rowIndex+1 >= measuredRowHeights.size()){
+				measuredRowHeights.add(0);
+			}
+			
+			
+			//TODO: deal with padding/spacing
+			if(newRow){
+				
+				rowIndex++;
+				currentRowHeight = h;
+				measuredRowHeights.set(rowIndex, w);
+			}else{
+				currentRowHeight += h;
+				if(w > this.measuredRowHeights.get(rowIndex)){
+					measuredRowHeights.set(rowIndex, w);
+				}
+			}
+			
+			if(currentRowHeight > maxRowHeight){
+				maxRowHeight = currentRowHeight;
+			}
+			
+		} //for each child
+		
+		
+		int finalHeight;
+		if(rowIndex > 0){
+			finalHeight = heightSize;
+		}else{
+			finalHeight = maxRowHeight;
+		}
+		
+		
+		int totalWidthAllRows = 0;
+		for(int i = 0; i < this.measuredRowHeights.size(); ++i){
+			totalWidthAllRows += measuredRowHeights.get(i);
+		}
+		
+		
+		int finalWidth = totalWidthAllRows + paddingW;
+		if(finalWidth > widthSize || MeasureSpec.EXACTLY == widthMode){
+			finalWidth = widthSize;
+		}
+		
+		
+		//log("setting measured dimensions: " + finalWidth + ", " + finalHeight);
+		setMeasuredDimension(finalWidth, finalHeight);
+		
+	}
 	
 	private void onMeasureHorizontal(int widthMeasureSpec, int heightMeasureSpec){
 		
@@ -148,7 +252,7 @@ public class FlowLayout extends ViewGroup {
 			if(child.getVisibility() == View.GONE) continue;
 			
 			//MarginGravityLayoutParams childLp = MarginGravityLayoutParams.fromLayoutParams(child.getLayoutParams());
-			MarginGravityLayoutParams childLp = (MarginGravityLayoutParams) child.getLayoutParams();
+			LinearLayout.LayoutParams childLp = (LinearLayout.LayoutParams) child.getLayoutParams();
 			
 			
 			
@@ -219,12 +323,88 @@ public class FlowLayout extends ViewGroup {
 	
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b){
-		onLayoutHorizontal(changed, l, t, r, b);
-		
-		//TODO: handle vertical case
+		if(orientation == HORIZONTAL)
+		{
+			onLayoutHorizontal(changed, l, t, r, b);
+		}
+		else
+	    {
+			onLayoutVertical(changed, l, t, r, b);
+		}
 	}
 
 	
+	private void onLayoutVertical(boolean changed, int l, int t, int r, int b) {
+				
+		int parentLeft = getPaddingLeft();
+		int parentRight = r - l - getPaddingRight();
+		int parentTop = getPaddingTop();
+		int parentBottom = b - t - getPaddingBottom();
+				
+		
+		int x = parentLeft;
+		int y = parentTop;
+		int rowIndex = 0;
+		
+		
+		for (int i = 0; i < getChildCount(); ++i){
+			final View child = getChildAt(i);
+			if(child.getVisibility() == View.GONE) continue;
+		
+			//MarginGravityLayoutParams childLp = MarginGravityLayoutParams.fromLayoutParams(child.getLayoutParams());
+			LinearLayout.LayoutParams childLp = (LinearLayout.LayoutParams) child.getLayoutParams();
+
+			if(childLp.gravity == -1)
+			{
+				childLp.gravity = Gravity.TOP | Gravity.START;
+			}
+			if(childLp.gravity == Gravity.TOP)
+			{
+				childLp.gravity = Gravity.TOP | Gravity.START;
+			}
+			if(childLp.gravity == Gravity.BOTTOM)
+			{
+				childLp.gravity = Gravity.BOTTOM | Gravity.START;
+			}
+			
+			if(y + child.getMeasuredHeight() + childLp.topMargin + childLp.bottomMargin > parentBottom){
+				
+				x += measuredRowHeights.get(rowIndex);
+				y = parentTop;
+				rowIndex++;
+				
+			}
+			
+			//log("row " + rowIndex + " child measured height: " + child.getMeasuredHeight());
+			
+			onLayoutRect.left = x + childLp.leftMargin;
+			onLayoutRect.right = x + measuredRowHeights.get(rowIndex);
+			onLayoutRect.top = y + childLp.topMargin;
+			onLayoutRect.bottom = onLayoutRect.top + child.getMeasuredHeight();
+			
+			
+			
+			//log("rect1: " + onLayoutRect.toShortString());
+			//log("width,height = " + child.getMeasuredWidth() + "," + child.getMeasuredHeight());
+			
+			//should only apply vertically
+			Gravity.apply(childLp.gravity, 
+					child.getMeasuredWidth(), 
+					child.getMeasuredHeight(), 
+					onLayoutRect, 
+					onLayoutRect2);
+		
+			//log("rect2: " + onLayoutRect2.toShortString());
+			
+			child.layout(
+					onLayoutRect2.left,
+					onLayoutRect2.top,
+					onLayoutRect2.right,
+					onLayoutRect2.bottom);
+			
+			y += childLp.topMargin + child.getMeasuredHeight() + childLp.bottomMargin;
+		}
+	}
 	private void onLayoutHorizontal(boolean changed, int l, int t, int r, int b) {
 		
 		
@@ -248,8 +428,13 @@ public class FlowLayout extends ViewGroup {
 			if(child.getVisibility() == View.GONE) continue;
 		
 			//MarginGravityLayoutParams childLp = MarginGravityLayoutParams.fromLayoutParams(child.getLayoutParams());
-			MarginGravityLayoutParams childLp = (MarginGravityLayoutParams) child.getLayoutParams();
+			LinearLayout.LayoutParams childLp = (LinearLayout.LayoutParams) child.getLayoutParams();
 
+			if(childLp.gravity == -1)
+			{
+				childLp.gravity = Gravity.TOP | Gravity.START;
+			}
+			
 			
 			if(x + child.getMeasuredWidth() + childLp.leftMargin + childLp.rightMargin > parentRight){
 				
@@ -289,43 +474,8 @@ public class FlowLayout extends ViewGroup {
 		}
 	}
 	
-	/*
-	private int getHorizontalChildWidthSpec(int widthMode, int widthSize, ViewGroup.LayoutParams childLp){
-	
-		
-		int childWidthSpec;
-		if(MeasureSpec.UNSPECIFIED == widthMode){
-			
-			if(childLp.width == ViewGroup.LayoutParams.WRAP_CONTENT
-					|| childLp.width == ViewGroup.LayoutParams.MATCH_PARENT){
-				
-				return MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.UNSPECIFIED);
-			}else{
-				
-				return MeasureSpec.makeMeasureSpec(childLp.width, MeasureSpec.EXACTLY);
-			}
-			
-		}else{
-			
-			if(childLp.width == ViewGroup.LayoutParams.WRAP_CONTENT
-					|| childLp.width == ViewGroup.LayoutParams.MATCH_PARENT
-					|| childLp.width > widthSize){
-			
-				childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.AT_MOST);
-			}else{
-				
-				childWidthSpec = MeasureSpec.makeMeasureSpec(childLp.width, MeasureSpec.EXACTLY);
-			}
-			
-			
-		}
-		return childWidthSpec;
-	}
-	
-	private int getHorizontalChildHeightSpec(){
-		
-	}
-	*/
-	
 	
 }
+
+
+
